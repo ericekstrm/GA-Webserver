4f0ead445b4f32fe995342aedd220448d7521f32
Eric Ekstrm <ericekstrm@gmail.com>
2015-09-04 22:32:39 +0200

    4/9 22:32
    Empty Project.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "hello world";
+        ?>
+    </body>
+</html>
